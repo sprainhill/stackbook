f78a1283ac3b5d708a00c73ce3ed7e3a9859c9d2
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2020-09-21 18:08:09 -0400

    Animate text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import "bulma"
 
 function App() {
+
+  const [ctaAnim, setCtaAnim] = useState('')
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="section">
+        <div className="container app-cont">
+          <div className={`sign-up ${ctaAnim}`}>
+            Sign Up
+          </div>
+          <div className='alrdy'>
+            already have a username?
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
