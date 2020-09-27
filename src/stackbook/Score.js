7a20032abd8a1a334ef9b2aead074c37ce7cbdec
Rory Murray <21082125+rorychristianmurray@users.noreply.github.com>
2020-09-27 12:56:35 -0400

    First pass radiks

--- src/stackbook/Score.js
+++ src/stackbook/Score.js
@@ -1,19 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import { Connect } from '@blockstack/connect'
 import { saveClicks } from "../assets/data-store"
 import { CLICKS_FILENAME } from "../assets/constants"
+import { useConnect } from '@blockstack/connect'
 import { UserSession } from "blockstack"
 import { appConfig } from "../assets/constants"
-import { fireEvent } from '@testing-library/react';
 
-const userSession = new UserSession({ appConfig })
-const putFileOptions = {
-  encrypt: false
-}
-const getFileOptions = {
-  decrypt: false
-}
+
+// const userSession = new UserSession({ appConfig })
+// const putFileOptions = {
+//   encrypt: false
+// }
+// const getFileOptions = {
+//   decrypt: false
+// }
+
 
 const Score = () => {
+  const { authOptions } = useConnect()
+const { userSession } = authOptions
+
+const userData = userSession.loadUserData()
+
   const [isPublic, setIsPublic] = useState(true)
   const [clicks, setClicks] = useState(0)
   const [gaiaClicks, setGaiaClicks] = useState(0)
@@ -24,11 +32,11 @@ const Score = () => {
   }
 
   const saveClicks = async () => {
-    await userSession.putFile(CLICKS_FILENAME, JSON.stringify(clicks), putFileOptions).then(console.log)
+    await userSession.putFile(CLICKS_FILENAME, JSON.stringify(clicks), {encrypt: false}).then(console.log)
   }
 
   const fetchClicks = async () => {
-    await userSession.getFile(CLICKS_FILENAME, getFileOptions).then(fileContents => {
+    await userSession.getFile(CLICKS_FILENAME, {decrypt: false}).then(fileContents => {
       const fc = JSON.parse(fileContents)
       setGaiaClicks(fc)
       
@@ -37,16 +45,19 @@ const Score = () => {
 
 
   return (
-    <div className="score-cols columns">
+    <Connect>
 
-      <div className="score-col column">
-        <div className="click-btn" onClick={handleClicks}>click me</div>
-        <div className="save-btn" onClick={fetchClicks}>get gaia clicks</div>
-        <div className="score">gaia clicks : {gaiaClicks}</div>
+      <div className="score-cols columns">
+  
+        <div className="score-col column">
+          <div className="click-btn" onClick={handleClicks}>click me</div>
+          <div className="save-btn" onClick={fetchClicks}>get gaia clicks</div>
+          <div className="score">gaia clicks : {gaiaClicks}</div>
+        </div>
+  
       </div>
-
-    </div>
-  );
+    </Connect>
+    );
 };
 
 export default Score;
